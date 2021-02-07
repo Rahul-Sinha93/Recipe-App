d9ff7a17dfd9653c51167cb600a9d9f999908d65
Rahul <rahulsinhar93@gmail.com>
2021-02-07 12:08:11 -0500

    Started the recipe App with the Search functionality and getting the data from the API with axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,32 @@
-// Global app controller
+import Search from './models/search';
+import { elements } from './views/base';   
+import * as searchView  from "./views/searchview"; 
 
-import x from './test';
 
-console.log(x);
-const y = 23;
-console.log(`here it is ${y}`)
+const state = {};
+
+const controlSearch = async () =>{
+    const query = searchView.getInput();
+
+    if(query){
+        state.search = new Search(query);
+        
+        searchView.clearInput();
+        searchView.clearResults();
+        
+        await state.search.getResults();
+        
+        searchView.randerResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit',e =>{
+    e.preventDefault();
+    controlSearch();
+})
+
+
+
+
+
+
